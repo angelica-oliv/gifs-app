73560df0f44cc37fab6e66fa3bd562d98f159613
Angelica Oliveira <angelica.liv@gmail.com>
2020-03-29 18:50:13 -0300

    gif viewmodel test

---
+++ app/src/test/java/com/angelicao/gifapp/giflist/GifListViewModelTest.kt
@@ -0,0 +1,66 @@
+package com.angelicao.gifapp.giflist
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.angelicao.repository.GifRepository
+import com.angelicao.repository.data.Gif
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+private val FAVORITE_GIF = Gif(id = "1",
+    url = "test.com",
+    title = "test",
+    favorite = true)
+private val GIF = Gif(id = "1",
+    url = "test.com",
+    title = "test",
+    favorite = false)
+private val GIF_LIST = listOf(FAVORITE_GIF, GIF)
+
+class GifListViewModelTest {
+    @JvmField @Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val testDispatcher = TestCoroutineDispatcher()
+    private val repository = mockk<GifRepository>(relaxed = true)
+    private lateinit var gifViewModel: GifListViewModel
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+        mockRepository()
+
+        gifViewModel = GifListViewModel(repository)
+    }
+
+    @Test
+    fun onFavoriteClicked_whenItemIsFavorite_favoriteIsRemoved() = runBlockingTest {
+        gifViewModel.onFavoriteClicked(FAVORITE_GIF)
+
+        coVerify { repository.removeFavoriteGif(FAVORITE_GIF) }
+        val favoriteAfter = gifViewModel.gifList.value?.first()?.favorite ?: true
+        assertFalse(favoriteAfter)
+    }
+
+    @Test
+    fun onFavoriteClicked_whenItemIsNotFavorite_favoriteIsAdded() = runBlockingTest {
+        gifViewModel.onFavoriteClicked(GIF)
+
+        coVerify { repository.favoriteGif(GIF) }
+        val favoriteAfter = gifViewModel.gifList.value?.first()?.favorite ?: true
+        assertTrue(favoriteAfter)
+    }
+
+    private fun mockRepository() {
+        coEvery { repository.getGIFs() } returns GIF_LIST
+    }
+}
