fca48baac4d2ba41a53fd306202a374031dffc8b
Angelica Oliveira <angelica.liv@gmail.com>
2020-04-14 10:52:10 -0300

    fix script

--- update-bitrise.sh
+++ update-bitrise.sh
@@ -3,6 +3,6 @@ curl -X GET "https://api.bitrise.io/v0.1/apps/$1/bitrise.yml" -H "Authorization:
 
 if ! git diff-index --quiet HEAD --; then
     git add bitrise.yml
-    git commit -m "updating bitrise.yml from build #$3
+    git commit -m "updating bitrise.yml from build #$3"
     git push
 fi
