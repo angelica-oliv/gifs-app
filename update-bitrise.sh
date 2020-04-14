4481d3155123b09ddfcab956fc8ad985df4fb9d2
Angelica Oliveira <angelica.liv@gmail.com>
2020-04-14 10:42:40 -0300

    creating script

---
+++ update-bitrise.sh
@@ -0,0 +1,8 @@
+> bitrise.yml
+curl -X GET "https://api.bitrise.io/v0.1/apps/$1/bitrise.yml" -H "Authorization: $2" > bitrise.yml
+
+if ! git diff-index --quiet HEAD --; then
+    git add bitrise.yml
+    git commit -m "updating bitrise.yml from build #$3
+    git push
+fi
