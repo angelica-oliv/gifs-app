d2be017e7d1d97d38eb7fe6c6e418c94ae8d70fb
Angelica Oliveira <angelica.liv@gmail.com>
2020-05-14 20:18:57 -0300

    init page implementation

--- network/src/main/java/com/angelicao/network/GifRemoteDataSource.kt
+++ network/src/main/java/com/angelicao/network/GifRemoteDataSource.kt
@@ -1,7 +1,23 @@
 package com.angelicao.network
 
+import androidx.paging.PositionalDataSource
+import com.angelicao.network.data.GifData
 import com.angelicao.network.service.GifAPI
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class GifRemoteDataSource(private val api: GifAPI) {
+class GifRemoteDataSource(private val api: GifAPI): PositionalDataSource<GifData>() {
     suspend fun getGIFs() = api.getTrendingGIFs()
+    override fun loadRange(params: LoadRangeParams, callback: LoadRangeCallback<GifData>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun loadInitial(
+        params: LoadInitialParams,
+        callback: LoadInitialCallback<GifData>
+    ) {
+        launch(Dispatchers.IO) {
+            callback.onResult(api.getTrendingGIFs().data, 0)
+        }
+    }
 }
--- network/src/main/java/com/angelicao/network/service/GifAPI.kt
+++ network/src/main/java/com/angelicao/network/service/GifAPI.kt
@@ -8,5 +8,7 @@ import retrofit2.http.Query
 interface GifAPI {
     // TODO pagination API when getting
     @GET("trending")
-    suspend fun getTrendingGIFs(@Query("api_key") apiKey: String = BuildConfig.GIPHY_API_KEY): GifResponse
+    suspend fun getTrendingGIFs(@Query("api_key") apiKey: String = BuildConfig.GIPHY_API_KEY,
+                                @Query("limit") limit: Int = 25,
+                                @Query("offset") offset: Int = 0): GifResponse
 }
